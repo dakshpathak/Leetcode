9b5b999d06d9b744d76ce7cc4c609a20e35a6402
Daksh Pathak <96343578+dakshpathak@users.noreply.github.com>
2022-12-17 11:03:19 +0530

    Time: 4 ms (87.10%), Space: 8.9 MB (21.49%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-int binaryCheck(vector<int>&nums,int st,int ed)
-{
-// small caluculation
-//check with neighnors
-int mid = st + (ed-st)/2;
-int n = nums.size()-1;
-bool left = mid ==0 || nums[mid-1]<=nums[mid];
-bool right = mid ==n || nums[mid+1]<=nums[mid];
-if(left&&right)
-{
-// returnong the mid index
-return mid;
-}
-else if(!left)//yani left wala number bda hai mtlb ye peak hoo skta
-{
-ed = mid -1;
-return binaryCheck(nums,st,ed);
-}else if(!right)// yaani right wal apart bda hai mtlb peak right me hoo skta
-{
-st = mid+1;
-return binaryCheck(nums,st,ed);
-}
-return -1;
-}
-int findPeakElement(vector<int>& nums) {
-int st = 0;
-int ed = nums.size()-1;
-return binaryCheck(nums,st,ed);
-}
+    int binaryCheck (vector <int>&nums, int ei, int si){
+        
+        int mid = (ei+si)/2;
+        int n  = nums.size()-1;
+        
+        bool left = mid == 0 || nums[mid-1]<= nums[mid];
+        bool right = mid == n || nums[mid+1]<=nums[mid];
+        
+        if (left && right){
+            return mid;
+        }
+        else if (!left){
+            ei = mid-1;
+            return binaryCheck (nums,ei,si);
+        }
+        else if (!right){
+            si = mid+1;
+            return binaryCheck (nums, ei, si);
+        }
+        return -1;
+    }
+    
+    int findPeakElement(vector<int>& nums) {
+        return binaryCheck(nums, nums.size()-1, 0);
+    }
 };
